5fa1b43881c1d2c1a7a32fc7c4ad98963cc4b623
Luke Thompson <luke.thompson@nine.com.au>
2024-08-28 19:59:30 +1000

    Simplified page two by repetitive repeated code and using grids

--- src/components/PageTwo.js
+++ src/components/PageTwo.js
@@ -4,17 +4,15 @@ import { formFields } from '../utils/formFields'
 const PageTwo = () => {
     return (
         <>
-            <h5 className="font-gotham font-[500] text-[21px] leading-[39px] mt-[32.85px]">To finalise your entry please fill in your details below</h5>
-            <div className='lg:w-[567px]'>
-                <div className='grid grid-cols-2 gap-x-[40px] gap-y-[18px] text-customForestGreen'>
-                    {formFields.map((field) => (
-                        <div className='flex flex-col w-[265px] mr-[40px]'>
-                            <label htmlFor={field.fieldType} className='text-[13px] leading-[23px]'>{field.label}*</label>
-                            <Field type={field.fieldType}  name={field.name} className="border border-customLighterGray outline-none w-full text-[13px] h-[36px] p-[6px]" required/>
-                            <ErrorMessage name={field.name} component="div" />
-                        </div>
-                    ))}
-                </div>
+            <h5 className="font-gotham font-[500] text-[21px] leading-[39px] mt-[32.85px] mb-[40.75px]">To finalise your entry please fill in your details below</h5>
+            <div className='grid grid-cols-2 gap-x-[40px] gap-y-[18px] text-customForestGreen lg:w-[567px]'>
+                {formFields.map((field) => (
+                    <div className='flex flex-col w-[265px] mr-[40px]'>
+                        <label htmlFor={field.fieldType} className='text-[13px] leading-[23px]'>{field.label}*</label>
+                        <Field type={field.fieldType}  name={field.name} className="border border-customLighterGray outline-none w-full text-[13px] h-[36px] p-[6px]" required/>
+                        <ErrorMessage name={field.name} component="div" />
+                    </div>
+                ))}
             </div>
             <button type='submit' className='mt-[38px] bg-customGreen w-[192px] h-[42px] font-gotham font-light text-white text-[20px] leading-[28px] hover:bg-white border-[1px] border-customGreen hover:text-customGreen transition ease-in-out duration-150'>Submit</button>
             <div className='text-[9px] leading-[12px] mt-[26px] text-center font-light lg:w-[621px] tracking-[-0.18px]'>
